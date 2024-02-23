b6ad1398b5387902245754861424ad398a1eff13
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2024-02-23 17:20:24 +0100

    feat: Update the url to modify the ghost links

--- updateConfig.js
+++ updateConfig.js
@@ -1,10 +1,12 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const contents = fs.readFileSync("config.production.json");
+const contents = fs.readFileSync('config.production.json');
 const config = JSON.parse(contents);
-const externalURL = process.env.url || process.env.RENDER_EXTERNAL_URL;
+
+// Render.com fails to set the environment variable, so we need to hardcode it
+// const externalURL = process.env.url || process.env.RENDER_EXTERNAL_URL;
+const externalURL = 'https://cms.vpnvision.com';
 if (externalURL) {
-  // update the URL in the config
   config.url = externalURL;
-  fs.writeFileSync("config.production.json", JSON.stringify(config, null, 2));
+  fs.writeFileSync('config.production.json', JSON.stringify(config, null, 2));
 }
